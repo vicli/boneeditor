2258a0cc8e39a2a6d8c3e81f01b70a47befa0b0a
vicli <vicli@18.189.92.203>
2012-11-26 12:58:27 -0500

    added files

---
+++ proj2/src/backend/Server.java
@@ -0,0 +1,10 @@
+package backend;
+
+public class Server {
+
+    /**
+     * @param args
+     */
+
+
+}
---
+++ proj2/src/gui/DocGUI.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class DocGUI {
+
+    /**
+     * @param args
+     */
+
+
+}

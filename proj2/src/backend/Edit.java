245588310e208ff331cdd817d428af663fed7df2
vicli <vicli@18.189.92.203>
2012-11-26 12:59:11 -0500

    added edit class

---
+++ proj2/src/backend/Edit.java
@@ -0,0 +1,13 @@
+package backend;
+
+public class Edit {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

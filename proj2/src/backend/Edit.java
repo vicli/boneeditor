7afa6d04a51c056e7d1501f9c3ff176cfd90bcd3
Miren Bamforth <mirrey.b@gmail.com>
2012-11-30 00:50:13 -0500

    Added new edit class

--- proj2/src/backend/Edit.java
+++ proj2/src/backend/Edit.java
@@ -1,9 +1,36 @@
 package backend;
 
 public class Edit {
-
+    final private char value;
+    private String owner
     /**
      * Edit class: includes buffer
      */
+    public Edit(char input, String clientName) {
+        value = input;
+        owner = clientName;
+    }
 
+    /**
+     * Returns the owner
+     * @return The owner of the edit
+     */
+    public String getOwner() {
+        return owner;
+    }
+    
+    /**
+     * Returns the value
+     * @return The value of this edit
+     */
+    public char getValue() {
+        return value;
+    }
+    
+    /**
+     * Changes the owner to the doc
+     */
+    public void changeOwner() {
+        owner = "doc";
+    }
 }

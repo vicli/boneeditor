cd76f9dd2837b71e25b6a7d0ad2fc023d65481d9
Miren Bamforth <mirrey.b@gmail.com>
2012-12-13 18:56:04 -0500

    cleaning up edit

--- proj2/src/backend/EditTest.java
+++ proj2/src/backend/EditTest.java
@@ -2,12 +2,23 @@ package backend;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 /**
  * Tests the Edit class
  */
 public class EditTest {
-    public void basicTest() {
+    @Test
+    public void ownerTest() {
+        //tests getOwner
         Edit edit1 = new Edit("a", "name");
         assertEquals("name", edit1.getOwner());
+    }
+    
+    @Test
+    public void valueTest() {
+        //tests getValue
+        Edit edit1 = new Edit("a", "name");
+        assertEquals("a", edit1.getValue());
     }
 }

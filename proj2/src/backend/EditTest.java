0015800d32f4dd12803e581f8cb1a66dc68b43b1
Miren Bamforth <mirrey.b@gmail.com>
2012-12-13 19:04:50 -0500

    edit and edittest are fully written and cleaned

--- proj2/src/backend/EditTest.java
+++ proj2/src/backend/EditTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
- * Tests the Edit class
+ * Tests the Edit class like a regular object
  */
 public class EditTest {
     @Test
@@ -20,5 +20,21 @@ public class EditTest {
         //tests getValue
         Edit edit1 = new Edit("a", "name");
         assertEquals("a", edit1.getValue());
+    }
+    
+    @Test
+    public void changeOwnerTest() {
+        //tests changeOwner
+        Edit edit = new Edit("m", "me");
+        assertEquals("me", edit.getOwner());
+        edit.changeOwner();
+        assertEquals("document", edit.getOwner());
+    }
+    
+    @Test
+    public void toStringTest() {
+        //tests toString
+        Edit edit1 = new Edit("a", "name");
+        assertEquals("a", edit1.toString());
     }
 }
